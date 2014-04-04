475ead5ddbb2c8cb42bace1bcf7f25081d52a162
Jason Berlinsky <jason@barefootcoders.com>
2014-04-04 01:22:57 -0400

    Allow specific tasks to be run

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Configuration
 
-All configuration should be handled in tasks.py so that the web runner knows how to handle changes
+All configuration should be handled in runner.py so that the web runner knows how to handle changes
+
+# TODO
+
+Abstract things into runner.py so that end users don't have to write this boilerplate...
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import runner
 index = 0
 filename = "index.html"
 infoList = []
-featureNames = []
+featureNames = [] # Appended to within included modules
 
 while os.path.isfile(runner.data_file_location() + filename):
     info = []
@@ -17,7 +17,20 @@ while os.path.isfile(runner.data_file_location() + filename):
 
 sys.path.append(os.path.join(os.path.dirname(__file__), 'tasks'))
 
-for task in runner.tasks():
+# Only run the tasks passed on the command line
+# HACK HACK HACK
+
+task_list = sys.argv[-1]
+if task_list == 'main.py':
+    task_list = runner.tasks()
+else:
+    task_list = str.split(task_list, ',')
+
+
+print "Running tasks: "
+print task_list
+
+for task in task_list:
     __import__(task)
 
 arff.dump(runner.arff_file_location(), infoList, relation="webpage_info", names=featureNames)
