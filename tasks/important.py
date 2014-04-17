c13804553bcd29211e673012e9d4d841a03a56c1
yuca <yuca@umich.edu>
2014-04-16 23:13:40 -0400

    abuse noisy a little bit, still generate arff within acceptable time range

--- tasks/important.py
+++ tasks/important.py
@@ -1,16 +1,9 @@
 from __main__ import funcs, attrNames
 import random
 
-attrNames.append('noisy50%')
-attrNames.append('noisy55%')
-attrNames.append('noisy60%')
-attrNames.append('noisy65%')
-attrNames.append('noisy70%')
-attrNames.append('noisy75%')
-attrNames.append('noisy80%')
-attrNames.append('noisy85%')
-attrNames.append('noisy90%')
-attrNames.append('noisy95%')
+for i in range(50, 100):
+	attrNames.append('noisy' + str(i) + '%')
+
 attrNames.append('important')
 
 def addNoisyAndImportanceAttr(info, index):
@@ -19,55 +12,11 @@ def addNoisyAndImportanceAttr(info, index):
 	if index < 153:
 		important = True
 
-	noisy50 = important
-	if random.randint(0,10) > 5:
-		noisy50 = not important
-	info.append(noisy50)
-
-	noisy55 = important
-	if random.randint(0,20) > 11:
-		noisy55 = not important
-	info.append(noisy55)
-
-	noisy60 = important
-	if random.randint(0,10) > 6:
-		noisy60 = not important
-	info.append(noisy60)
-
-	noisy65 = important
-	if random.randint(0,20) > 13:
-		noisy65 = not important
-	info.append(noisy65)
-
-	noisy70 = important
-	if random.randint(0,10) > 7:
-		noisy70 = not important
-	info.append(noisy70)
-
-	noisy75 = important
-	if random.randint(0,20) > 15:
-		noisy75 = not important
-	info.append(noisy75)
-
-	noisy80 = important
-	if random.randint(0,10) > 8:
-		noisy80 = not important
-	info.append(noisy80)
-
-	noisy85 = important
-	if random.randint(0,20) > 17:
-		noisy85 = not important
-	info.append(noisy85)
-
-	noisy90 = important
-	if random.randint(0,10) > 9:
-		noisy90 = not important
-	info.append(noisy90)
-
-	noisy95 = important
-	if random.randint(0,20) > 19:
-		noisy95 = not important
-	info.append(noisy95)
+	for i in range(50, 100):
+		if random.randint(0,100) < i:
+			info.append(important)
+		else:
+			info.append(not important)
 
 	info.append(important)
 
