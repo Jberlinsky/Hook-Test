2d692c113c53f4a2f5d56e99865f0495e94875d5
yuca <yuca@umich.edu>
2014-04-16 23:03:00 -0400

    oops, minor change

--- tasks/important.py
+++ tasks/important.py
@@ -20,52 +20,52 @@ def addNoisyAndImportanceAttr(info, index):
 		important = True
 
 	noisy50 = important
-	if random.randint(0,10) < 5:
+	if random.randint(0,10) > 5:
 		noisy50 = not important
 	info.append(noisy50)
 
 	noisy55 = important
-	if random.randint(0,20) < 11:
+	if random.randint(0,20) > 11:
 		noisy55 = not important
 	info.append(noisy55)
 
 	noisy60 = important
-	if random.randint(0,10) < 6:
+	if random.randint(0,10) > 6:
 		noisy60 = not important
 	info.append(noisy60)
 
 	noisy65 = important
-	if random.randint(0,20) < 13:
+	if random.randint(0,20) > 13:
 		noisy65 = not important
 	info.append(noisy65)
 
 	noisy70 = important
-	if random.randint(0,10) < 7:
+	if random.randint(0,10) > 7:
 		noisy70 = not important
 	info.append(noisy70)
 
 	noisy75 = important
-	if random.randint(0,20) < 15:
+	if random.randint(0,20) > 15:
 		noisy75 = not important
 	info.append(noisy75)
 
 	noisy80 = important
-	if random.randint(0,10) < 8:
+	if random.randint(0,10) > 8:
 		noisy80 = not important
 	info.append(noisy80)
 
 	noisy85 = important
-	if random.randint(0,20) < 17:
+	if random.randint(0,20) > 17:
 		noisy85 = not important
 	info.append(noisy85)
 
 	noisy90 = important
-	if random.randint(0,10) < 9:
+	if random.randint(0,10) > 9:
 		noisy90 = not important
 	info.append(noisy90)
 
 	noisy95 = important
-	if random.randint(0,20) < 19:
+	if random.randint(0,20) > 19:
 		noisy95 = not important
 	info.append(noisy95)
 
